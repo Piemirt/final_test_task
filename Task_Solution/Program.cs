09750c4e926bb8fb2a6a491926db56ad67e01952
Kirill <konliri@gmail.com>
2022-11-22 18:55:32 +0800

    Написание основного кода решения задачи

--- Task_Solution/Program.cs
+++ Task_Solution/Program.cs
@@ -1,2 +1,30 @@
 ﻿Console.Clear();
 
+string[] firstArray = new string[4] { "hello", "2", "world", ":-)" };
+string[] secondArray = new string[firstArray.Length];
+
+ArrayCheck(firstArray, secondArray);
+PrintArray(secondArray);
+
+
+void ArrayCheck(string[] firstArray, string[] secondArray)
+{
+    int n = 3;
+    int j = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= n)
+        {
+            secondArray[j] = firstArray[i];
+            j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
